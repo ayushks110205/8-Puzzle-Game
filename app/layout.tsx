aa23eaf20f7f04078d98629191248f0f2892f815
Ayushks110205 <classsciencea@gmail.com>
2026-03-10 04:01:50 +0530

    Updated pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,17 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "8 Puzzle Game",
   description: "Interactive 8 Puzzle Game with AI Solver",
+
+  manifest: "/manifest.json",
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-96x96.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
